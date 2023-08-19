085c5bc63d62588f5d8e4abdc9b63d15233ab7ab
Gabriel Nathan Almeida Silva Gabriel <76185909+GabrielNathan12@users.noreply.github.com>
2023-08-19 17:46:59 -0300

    feat? criacao do projeto Angular

---
+++ back-end/Serve.php
@@ -0,0 +1,8 @@
+<?php 
+    $json =
+		'[{"nome":"Jason Jones", "idade":38, "sexo": "M"},'.
+		'{"nome":"Ada Pascalina", "idade":35, "sexo": "F"},'.
+		'{"nome":"Delphino da Silva", "idade":26, "sexo": "M"}'.
+		']';
+    echo "$json";
+?>
