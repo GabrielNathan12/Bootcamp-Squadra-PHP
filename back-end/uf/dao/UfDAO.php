2e3cc96e4cda4fc21770bfe02f117db1c4450453
Gabriel Nathan <gabrielnathanmiw@gmail.com>
2023-09-02 14:32:33 -0300

    feat: QueryParans de UF feito

--- back-end/uf/dao/UfDAO.php
+++ back-end/uf/dao/UfDAO.php
@@ -7,16 +7,91 @@ class UfDAO {
         $this->conexao = $conexao;
     }
 
-    public function listarTodosUFs() {
-        $sql = "SELECT * FROM TB_UF";
+public function listarTodosUFs() {
+    if (!empty($_GET)) {
+        $sql = "SELECT * FROM TB_UF WHERE 1=1";
         
+        if (isset($_GET['codigoUF'])) {
+            $codigoUF = intval($_GET['codigoUF']);
+            $sql .= " AND codigoUF = :codigoUF";
+        }
+
+        if (isset($_GET['nome'])) {
+            $nome = $_GET['nome'];
+            $sql .= " AND nome = :nome";
+        }
+
+        if (isset($_GET['sigla'])) {
+            $sigla = $_GET['sigla'];
+            $sql .= " AND sigla = :sigla";
+        }
+
+        if (isset($_GET['status'])) {
+            $status = intval($_GET['status']);
+            $sql .= " AND status = :status";
+        }
+
         $stmt = $this->conexao->prepare($sql);
+
+        if (isset($codigoUF)) {
+            $stmt->bindParam(':codigoUF', $codigoUF);
+        }
+        if (isset($nome)) {
+            $stmt->bindParam(':nome', $nome);
+        }
+        if (isset($sigla)) {
+            $stmt->bindParam(':sigla', $sigla);
+        }
+        if (isset($status)) {
+            $stmt->bindParam(':status', $status);
+        }
+
         $stmt->execute();
 
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        return $result;
+        if($codigoUF){
+            $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+            if(!$resultado){
+                return [];
+            }else{
+                return $resultado;
+            }
+        }
+        else if($sigla){
+            $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+            if(!$resultado){
+                return [];
+            }
+            else {
+                return $resultado;
+            }
+        }
+        else if($status){
+            $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+          
+            if(!$resultado){
+                return [];
+            }
+            else{
+                return $resultado;
+            }
+        }
+        else{
+            $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $resultado;
+        }
     }
+    else {
+        $sql = "SELECT * FROM TB_UF";
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->execute();
+        $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    return $resultado;
+}
+
+    
+    
 
     public function criarUf(UF $uf){
         $nome = $uf->getNomeUf();
