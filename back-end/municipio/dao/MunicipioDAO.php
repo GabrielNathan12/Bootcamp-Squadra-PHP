8b8b1eb85dc5030135b50bf7e5f7abf110ea5690
Gabriel Nathan <gabrielnathanmiw@gmail.com>
2023-09-02 17:33:43 -0300

    feat: Query parans de Municipio feito

--- back-end/municipio/dao/MunicipioDAO.php
+++ back-end/municipio/dao/MunicipioDAO.php
@@ -6,11 +6,86 @@
             $this->conexao = $conexao;
         }
         public function listarMunicipios(){
-            $sql = "SELECT * FROM TB_MUNICIPIO";
-            $stmt = $this->conexao->prepare($sql);
-            $stmt->execute();
-            $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            return $resultado;
+
+            if(!empty($_GET)){
+                $sql = "SELECT * FROM TB_MUNICIPIO WHERE 1=1";
+
+                if(isset($_GET['codigoMunicipio'])){
+                    $codigoMunicipio = intval($_GET['codigoMunicipio']);
+                    $sql .= " AND codigoMunicipio = :codigoMunicipio";
+                }
+
+                if(isset($_GET['codigoUF'])){
+                    $codigoUF = intval($_GET['codigoUF']);
+                    $sql .= " AND codigoUF = :codigoUF";
+                }
+
+                if(isset($_GET['nome'])){
+                    $nome = $_GET['nome'];
+                    $sql .= " AND nome = :nome";
+                }
+
+                if(isset($_GET['status'])){
+                    $status = intval($_GET['status']);
+                    $sql .= " AND status = :status";
+                }
+                $stmt = $this->conexao->prepare($sql);
+
+                if(isset($codigoMunicipio)){
+                    $stmt->bindParam(':codigoMunicipio',$codigoMunicipio);
+                }
+                if(isset($codigoUF)){
+                    $stmt->bindParam(':codigoUF', $codigoUF);
+                }
+                if(isset($nome)){
+                    $stmt->bindParam(':nome', $nome);
+                }
+                if(isset($status)){
+                    $stmt->bindParam(':status', $status);
+                }
+
+                $stmt->execute();
+                
+                if($codigoMunicipio){
+                    $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+                    if(!$resultado){
+                        return [];
+                    }else {
+                        return $resultado;
+                    }
+                }
+                else if($codigoUF){
+                    $resultado = $stmt->fetch(PDO::FETCH_ASSOC);
+                    if(!$resultado){
+                        return [];
+                    }else{
+                        return $resultado;
+                    }
+                }
+                else if($nome){
+                    $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    if(!$resultado){
+                        return [];
+                    }else {
+                        return $resultado;
+                    }
+                }
+                else if($status){
+                    $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    if(!$resultado){
+                        return [];
+                    }else {
+                        return $resultado;
+                    }
+                }
+            }
+            else {
+                $sql = "SELECT * FROM TB_MUNICIPIO";
+                $stmt = $this->conexao->prepare($sql);
+                $stmt->execute();
+                $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                return $resultado;
+            }
         }
         private  function verificaCodigoUF($codigoUF){
             $sql = "SELECT COUNT(*) FROM TB_UF WHERE codigoUF = :codigoUF";
