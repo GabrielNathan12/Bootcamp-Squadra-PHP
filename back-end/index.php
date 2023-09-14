3e978d4bd644866f2c43836698363ec1cc5e7624
Gabriel Nathan <gabrielnathanmiw@gmail.com>
2023-09-14 11:30:27 -0300

    feat: configurações do do BD no arquivo .env

--- back-end/index.php
+++ back-end/index.php
@@ -1,6 +1,30 @@
 <?php
+   
+   $arqEnv = __DIR__ . '/.env';
 
-   phpinfo();
+   if (file_exists($arqEnv)) {
+      $linhas = file($arqEnv, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+      if ($linhas) {
+         foreach ($linhas as $linha) {
+               list($chaves, $valor) = explode('=', $linha, 2);
+               $chaves = trim($chaves);
+               $valor = trim($valor);
+               putenv("$chaves=$valor");
+               $_ENV[$chaves] = $valor;
+               $_SERVER[$chaves] = $valor;
+         }
+      }
+   }
+   else {
+      die('arquivo de configuração do banco de dados ".env" não encontrado.');
+   }
+
+   $dbNome = getenv('DBNOME');
+   $dbHost = getenv('DBHOST');
+   $dbUser = getenv('DBUSER');
+   $dbSenha = getenv('DBSENHA');
+   
+
 
        
 
--- back-end/sistema/ConectarBD.php
+++ back-end/sistema/ConectarBD.php
@@ -1,13 +1,19 @@
 <?php 
-    //$arquivoEnv = parse_ini_file('./.env', true);
+    include_once('../index.php');
+    
+    $dbNome = $dbNome;
+    $dbHost = $dbHost;
+    $dbUser = $dbUser;
+    $dbSenha = $dbSenha;
 
-    $dbNome = 'postgres';
-    $dbHost = 'localhost';
-    $dbUser = 'postgres';
-    $dbSenha = 'welcome1';
-
-    $conexao = new PDO('pgsql:dbname='.$dbNome .';host='.$dbHost, $dbUser, $dbSenha);
-    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conexao->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    try{
+        $conexao = new PDO('pgsql:dbname='.$dbNome .';host='.$dbHost, $dbUser, $dbSenha);
+        $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conexao->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    }
+    catch(Exception $e){
+        echo "Erro ao conectar ao banco de dados: $e";
+    }
+    
     
     
